80f4e8f6df194799e64e7d109700967b977ad563
Ezeuju <ezeuju1919@gmail.com>
2023-07-25 16:59:12 +0100

    webpack update

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,14 +1,12 @@
-const path = require('path');
-
+const path = require("path");
 
 module.exports = {
   mode: 'production',
   entry: {
-    index: path.resolve(__dirname, 'js/dashboard_main.js'),
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
   },
-
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-  }
-}
+    filename: 'bundle.js',
+  },
+};
